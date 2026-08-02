5da4da4993135c1948362192b0af14c9422b579b
onimusha-dev <musaddikulislam007@gmail.com>
2026-08-02 19:29:07 +0530

    damn

---
+++ 5. Longest Palindromic Substring/better.ts
@@ -0,0 +1,15 @@
+const s = "babad"
+
+function longestPalindrome(s: string): string {
+    let subString = ""
+
+    for (let left = 0; left < s.length - 1; left++) {
+        
+
+        
+    }
+
+    return ""
+};
+
+console.log(longestPalindrome(s));
